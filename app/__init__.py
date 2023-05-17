97bd6231687081777307951d8698fcbc2bd97e62
todo_056n_user <sanjanat8402@gmail.com>
2023-05-17 14:18:20 +0530

    database credentials updated

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import psycopg2
 
 app = Flask(__name__)
 postgres = psycopg2.connect(
-        host="https://github.com/sanjanat84/campusworkshop.git",
+        host="@dpg-chi7u75269vf5qbbngag-a.oregon-postgres.render.com",
         database="todo_056n",
         user="todo_056n_user",
         password="szxvX8ZpQfdUNqqghzmEuUN8yTLYVmYU")
